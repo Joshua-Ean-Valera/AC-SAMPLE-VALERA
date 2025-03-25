3f7935b9658d759080289e3cc9144fdfb150bbcc
LAPTOP-3RR9TLQA\Joshua Ean <jovalera@my.cspc.edu.ph>
2025-03-25 15:13:42 +0800

    tard

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("HELLO MY KNEEGEAR!!!")
+st.title("Sorry na My Baby")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Patawarin mo na ako kahit parehas naman tayong may mali. :(("
 )
